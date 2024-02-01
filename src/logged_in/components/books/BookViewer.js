0bd6f8f9e43e0b911b6eb155e1460bee809d15de
mizu-orient <mizu.orient@gmail.com>
2024-02-01 04:59:24 +0000

    a

--- src/logged_in/components/books/BookViewer.js
+++ src/logged_in/components/books/BookViewer.js
@@ -1,25 +1,68 @@
-import React, { Fragment, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
 import dummyBooks from "../../../shared/dummy_data/dummyBooks.json";
+import defaultCover from "./images/notavailable.png";
 
 const BookViewer = (props) => {
   const { selectBookViewer } = props;
+  const [title, setTitle] = useState([]);
+  const [images, setImages] = useState([]);
   const bookId = localStorage.getItem("currentBookId");
-  const bookData = dummyBooks.find((book) => book.bookId === parseInt(bookId));
-  const dummyImages = bookData ? bookData.dummyImages : [];
+  const history = useHistory();
 
   useEffect(() => {
     selectBookViewer();
-  }, [selectBookViewer]);
+    const bookData = dummyBooks.find(
+      (book) => book.bookId === parseInt(bookId)
+    );
+    setImages(bookData ? bookData.dummyImages : defaultCover);
+    setTitle(bookData.title);
+  }, [selectBookViewer, bookId]);
 
-  console.log("DM", dummyImages);
+  const goBack = () => {
+    history.goBack();
+  };
+
+  const goForward = () => {
+    const currentPage = history.location.pathname.split("/")[2];
+    if (currentPage < images.length - 1) {
+      history.push(`/image/${parseInt(currentPage) + 1}`);
+    }
+  };
+
+  return (
+    <Router>
+      <div>
+        <h2>{title}</h2>
+        {images.map((image, index) => (
+          <div key={index}>
+            <Link to={`/image/${index}`}>Page{index + 1}</Link>
+          </div>
+        ))}
+        <button onClick={goBack}>戻る</button>
+        <button onClick={goForward}>進む</button>
+        <Route
+          path="/image/:id"
+          render={(props) => <ImagePage {...props} images={images} />}
+        />
+      </div>
+    </Router>
+  );
+};
+
+const ImagePage = (props) => {
+  const { id } = props.match.params;
+  const { images } = props;
+  const image = images[parseInt(id)];
 
   return (
     <div>
-      <h2>BookViewer : {bookId}</h2>
-      {dummyImages &&
-        dummyImages.map((image, index) => (
-          <img key={index} src={image} alt={`book image ${index}`} />
-        ))}
+      <img src={image ? image : defaultCover} alt="Book image" />
     </div>
   );
 };
--- src/logged_in/components/books/CreateBook.js
+++ src/logged_in/components/books/CreateBook.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Popup from "reactjs-popup";
+import "./CreateBook.css";
 
 import defaultCover from "./images/notavailable.png";
 import dummyBooks from "../../../shared/dummy_data/dummyBooks.json";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <img src={book.coverImage} alt={`Cover of book ${book.bookId}`} />
+      <div className="book-details">
+        <h3>{book.title}</h3>
+        <p>{book.description}</p>
+        <p>{book.ratio}</p>
+      </div>
+    </div>
+  );
+};
 
 const CreateBook = (props) => {
   const { selectCreateBook } = props;
@@ -19,6 +33,9 @@ const CreateBook = (props) => {
   const retrieveBooks = () => {
     const selectedBooks = dummyBooks.map((book) => ({
       bookId: book.bookId,
+      title: book.title, // タイトルを追加
+      description: book.description,
+      ratio: book.ratio,
       coverImage: book.coverImage ? book.coverImage : defaultCover,
     }));
     setBooks(selectedBooks);
@@ -27,34 +44,9 @@ const CreateBook = (props) => {
 
   const BookDisplay = () => {
     return (
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-        }}
-      >
+      <div className="book-display">
         {books.map((book, index) => (
-          <div
-            key={index}
-            style={{
-              flex: "1 0 30%", // 3つまで横並びになるように調整
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between", // 本の位置を揃える
-              marginBottom: "10px",
-              marginRight: "1.5%", // 余白を設定
-            }}
-          >
-            <div style={{ marginRight: "10px" }}>{book.bookId}</div>
-            <div>
-              <img
-                src={book.coverImage}
-                alt={`Cover of book ${book.bookId}`}
-                style={{ width: "50px", height: "auto" }}
-              />
-            </div>
-          </div>
+          <BookCard key={index} book={book} />
         ))}
       </div>
     );
